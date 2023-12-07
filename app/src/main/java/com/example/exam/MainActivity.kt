b74b7599944550864e77349dd6e53aed1c48256b
Dev <dev.12092003@gmail.com>
2023-12-07 22:00:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/exam/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.exam
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var btn: Button =findViewById(R.id.btn1)
+        btn.setOnClickListener()
+        {
+            var intent: Intent = Intent(this,MapsActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
